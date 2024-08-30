ab298a84f528f11a73c5934aa81e8b033e0bff69
Ruben <ruben@zkr.nl>
2024-08-30 14:16:05 +0200

    gitignore secrets

--- ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
+++ ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70418ba9722a4b17911969d0f9f4e735e6accb0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2b348b994955fdf9955006b457ec21798e0eb583")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
