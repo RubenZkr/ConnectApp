8bda1a9650772f38372f024637e6a500d0474e74
Ruben <ruben@zkr.nl>
2024-08-30 14:02:25 +0200

    Adjust config

--- ConnectApp/ConnectApp/Program.cs
+++ ConnectApp/ConnectApp/Program.cs
@@ -1,25 +1,85 @@
+using Microsoft.AspNetCore.Authentication.OpenIdConnect;
+using Microsoft.Identity.Web;
+using Microsoft.Identity.Web.UI;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var azureAdSection = builder.Configuration.GetSection("AzureAd");
+if (azureAdSection == null)
+{
+    throw new ArgumentNullException("AzureAd section is missing in configuration.");
+}
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var scopes = builder.Configuration.GetSection("AzureAd:Scopes").Value;
+if (string.IsNullOrEmpty(scopes))
+{
+    throw new ArgumentNullException("AzureAd:Scopes section is missing or empty in configuration.");
+}
+
+builder.Services.AddAuthentication(OpenIdConnectDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApp(azureAdSection)
+    .EnableTokenAcquisitionToCallDownstreamApi(scopes.Split(" "))
+    .AddInMemoryTokenCaches();
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("AdminPolicy", policy => policy.RequireRole("Admin"));
+    options.AddPolicy("UserPolicy", policy => policy.RequireRole("User"));
+});
+
+builder.Services.AddControllersWithViews();
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMvc()
+    .AddMicrosoftIdentityUI(); 
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAllOrigins",
+        builder =>
+        {
+            builder.WithOrigins("http://localhost:8080", "https://localhost:5050")
+                .AllowAnyHeader()
+                .AllowAnyMethod()
+                .AllowCredentials();
+        });
+});
+
+
+
 var app = builder.Build();
 
+app.UseCors("AllowAllOrigins");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
 
 app.UseHttpsRedirection();
+app.UseStaticFiles();
 
+app.UseRouting();
+
+app.UseAuthentication();
 app.UseAuthorization();
 
-app.MapControllers();
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapRazorPages();
+    endpoints.MapBlazorHub();
+});
 
 app.Run();
--- ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
+++ ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee791d3541e633fe7efd01b501164280f4cfa28e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c70418ba9722a4b17911969d0f9f4e735e6accb0")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
