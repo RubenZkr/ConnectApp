b028febab244d3a1554b30d999198fbfafcc52c0
Ruben <ruben@zkr.nl>
2024-08-30 14:49:35 +0200

    ignore and adjustments

--- ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
+++ ConnectApp/ConnectApp/obj/Debug/net8.0/ConnectApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab298a84f528f11a73c5934aa81e8b033e0bff69")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e8ffa1a5015cef4ba0375513e47ebf163cdb34ab")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConnectApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
